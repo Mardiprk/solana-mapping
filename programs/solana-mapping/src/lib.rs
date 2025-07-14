262b13cf3328b2cdd4d8f8989b3eb458364954ae
Mardiprk <mk1659770@gmail.com>
2025-07-14 12:06:02 +0000

    simple mapping working

--- programs/solana-mapping/src/lib.rs
+++ programs/solana-mapping/src/lib.rs
@@ -3,14 +3,64 @@ use anchor_lang::prelude::*;
 declare_id!("8zy2dNPzUPX3kiqRAbBF4YZV3gvzoijmHbvyByM77rrb");
 
 #[program]
-pub mod solana_mapping {
+pub mod simple_map {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.authority = ctx.accounts.authority.key();
+        user_account.data = data;
+        Ok(())
+    }
+
+    pub fn set(ctx: Context<Set>, new_value: u64) -> Result<()> {
+        let account = &mut ctx.accounts.user_account;
+        require_keys_eq!(
+            account.authority,
+            ctx.accounts.authority.key(),
+            CustomError::UnAuthorized
+        );
+        account.data = new_value;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + 32 + 8,
+        seeds = [b"user", authority.key().as_ref()],
+        bump,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Set<'info> {
+    #[account(
+        mut,
+        seeds = [b"user", authority.key().as_ref()],
+        bump
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct UserAccount {
+    pub authority: Pubkey,
+    pub data: u64,
+}
+
+#[error_code]
+enum CustomError {
+    #[msg("you're not authorized")]
+    UnAuthorized,
+}
--- tests/solana-mapping.ts
+++ tests/solana-mapping.ts
@@ -1,16 +1,48 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { SolanaMapping } from "../target/types/solana_mapping";
+import { SimpleMap} from "../target/types/simple_map";
 
-describe("solana-mapping", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("Simple Map", () => {
+  const  provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.solanaMapping as Program<SolanaMapping>;
+  const program = anchor.workspace.SimpleMap as Program<SimpleMap>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Initialize", async () => {
+    const user = provider.wallet;
+
+    const [userPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("user"), user.publicKey.toBuffer()],
+      program.programId
+    );
+
+    await program.methods.initialize(new anchor.BN(23)).accounts({
+      userAccount: userPda,
+      authority: user.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc();
+
+    const account = await program.account.userAccount.fetch(userPda);
+    console.log("📦 PDA:", userPda.toBase58());
+    console.log("✅ Stroed Data:", account.data.toString());
+
   });
-});
+
+  it("Assigning new value", async () => {
+    const user = provider.wallet;
+
+    const [userPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("user"), user.publicKey.toBuffer()],
+      program.programId
+    );
+
+    await program.methods.set(new anchor.BN(99)).accounts({
+      userAccount: userPda,
+      authority: user.publicKey
+    }).rpc();
+
+    const updated = await program.account.userAccount.fetch(userPda);
+    console.log("🔁 Updated Value: ", updated.data.toString());
+
+  });
+}); 
